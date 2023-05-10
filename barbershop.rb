8aadca00f081d08da62662c87b9697f57f0f76b4
soutajo <roninghost42@gmail.com>
2023-05-10 23:57:10 +0300

    Add post method handling

--- barbershop.rb
+++ barbershop.rb
@@ -3,3 +3,14 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = 'Thank you!'
+  @message = "Dear #{@user_name}, we`ll be waiting for you at #{@date_time}"
+
+  erb :message
+end
