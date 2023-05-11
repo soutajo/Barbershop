b4bfbb897b88adaf8bbba20a1f105a70001571a4
soutajo <roninghost42@gmail.com>
2023-05-11 17:40:52 +0300

    Save information to users.txt. Add .gitignore

--- barbershop.rb
+++ barbershop.rb
@@ -12,5 +12,9 @@ post '/' do
   @title = 'Thank you!'
   @message = "Dear #{@user_name}, we`ll be waiting for you at #{@date_time}"
 
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}"
+  f.close
+
   erb :message
 end
